2e2507a206430c0c71f90d17f36a3c71aaf13d06
desenvolvedorWA <desenvolvedor@waproject.com.br>
2021-06-18 15:51:36 -0300

    [New Features]: Botões de incremente de perguntas parcialmente funcionando

--- js/cardController.js
+++ js/cardController.js
@@ -1,15 +1,13 @@
-const createQuestion = () => {
-  const createDiv = document.createElement('div');
-
-  createDiv.innerHTML = ` 
-    <div class="row justify-content-center form-group">
+const CardComponent = (id) => (
+  ` 
+    <div class="row justify-content-center form-group" id='card-${id}'>
       <div class="col-xl-6 col-md-6 offset-md-1">
         <div class="card border-left-${colorCard()} shadow h-100 py-2">
           <div class="card-body">
             <div class="row no-gutters align-items-center">
               <div class="col mr-2">
                 <div class="text-xs font-weight-bold text-primary text-uppercase mb-10">
-                    Faça a sua pergunta :)
+                  Faça a sua pergunta :)
                 </div>
                 <div class="h5 mb-0 font-weight-bold text-gray-800">
                   <form class="user">
@@ -28,17 +26,40 @@ const createQuestion = () => {
             </div>
           </div>
         </div>
-      </div>										
-      <div class="col-md-1 mb-1">
-        <a href="#" class="btn btn-success btn-circle"  onclick="createQuestion()">
-          <i class="fas fa-plus"></i>
-        </a>
       </div>
+      ${ButtonsComponent(id)}
     </div>
-  `;
+  `
+);
+
+const ButtonsComponent = (id) => ( 
+  `
+    <div class="col-md-1 mb-1" id="buttons-${id}">
+      <a href="#" class="btn btn-success btn-circle" id="${id}" onclick="createQuestion(this.id)">
+        <i class="fas fa-plus"></i>
+      </a>
+      <a href="#" class="btn btn-danger btn-circle" id="${id}"  onclick="deleteQuestion(this.id)">
+        <i class="fas fa-trash"></i>
+      </a>
+    </div>
+  `
+);
+
+const createQuestion = (id) => {
+  const createDiv = document.createElement('div');
+
+  document.getElementById(id).style.display  = 'none';
+
+  createDiv.innerHTML = CardComponent(++id);
 
   const questions = document.getElementById('questions');
   questions.appendChild(createDiv);
+  
+}
+
+const deleteQuestion = (id) => {
+  document.getElementById(`card-${id}`).remove();
+  document.getElementById(--id).style = 'block';
 }
 
 
@@ -54,8 +75,6 @@ const colorCard = () => {
   ];
 
   const random = Math.floor(Math.random() * 7);
-
-  console.log(random, colors[random]);
 
   return colors[random];
 }
